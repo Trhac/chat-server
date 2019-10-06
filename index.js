ab7508d6df67ea5e7bf667eb2273b16073318fdb
Trhac <trhac@live.com>
2019-10-06 16:15:06 +0200

    Pridane uzivatelske meno

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+var message = 'Vitajte na serveri';
+var timestmp = Date.now();
+var userName = 'admin';
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+)
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
-    console.log(req.body);
+    var JSON = {
+	time: timestmp,
+	sprava: message,
+	user: userName
+}
+    res.send(JSON);
+});
+
+app.post('/',(req, res) => {
+	res.send('Post metoda');
+	message = req.body.sprava;
+	timestmp = Date.now();
+	userName = req.body.user;
 });
 
 app.get('/api/courses', (req, res) => {
 	res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000..'));
+app.listen(4000, () => console.log('Listening on port 3000..'));
